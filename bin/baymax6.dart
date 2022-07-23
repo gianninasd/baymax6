2821766194093a97e429fdcb8f3a17da4c0bb92b
gianninasd <angelone55@hotmail.com>
2022-07-23 19:05:22 -0400

    added default dart console app code

---
+++ bin/baymax6.dart
@@ -0,0 +1,5 @@
+import 'package:baymax6/baymax6.dart' as baymax6;
+
+void main(List<String> arguments) {
+  print('Hello world: ${baymax6.calculate()}!');
+}
---
+++ test/baymax6_test.dart
@@ -0,0 +1,8 @@
+import 'package:baymax6/baymax6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
