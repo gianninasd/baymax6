62ebc0025e395c9687e10ed6d8228232bfec8ba2
gianninasd <angelone55@hotmail.com>
2022-07-23 19:42:10 -0400

    add basics

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+# Sample Dart HTTP client
+A sample command-line application with an entrypoint in `bin/`, library code in `lib/`, and example unit test in `test/`.
+
+## Pre-requisites
+- Install Git 2.32.x
+- Install Dart 2.17.x
+
+Now to get the code and install the dependencies perform the following commands:
+- `git clone https://github.com/gianninasd/baymax6.git`
+- `cd baymax6`
+- Open a console and run the following Git commands to configure Git credentials for this repo
+  - `git config --local user.email "<your email>"`
+  - `git config --local user.name "<your name>"`
+  - `git config --local credential.username "<your git username>"`
--- bin/baymax6.dart
+++ bin/baymax6.dart
@@ -1,5 +1,43 @@
-import 'package:baymax6/baymax6.dart' as baymax6;
+import 'dart:convert';
+import 'dart:io';
 
+//import 'package:baymax6/baymax6.dart' as baymax6;
+
+// run the specific business command
+void executeCommand(String command) {
+  switch (command) {
+    case 'G':
+      print('not implemented yet');
+      break;
+    case 'L':
+      print('not implemented yet');
+      break;
+  }
+}
+
+//
+String readCommand() {
+  print('Enter command [G / L / Q]: ');
+  var charCode = stdin.readByteSync();
+  //print('it was: $charCode');
+
+  var x = AsciiDecoder();
+  var cmd = x.convert([charCode]);
+  print('it was: $cmd');
+  return cmd.toUpperCase();
+}
+
+// application starts here
 void main(List<String> arguments) {
-  print('Hello world: ${baymax6.calculate()}!');
+  print('----------------------------------------');
+  print('Running Baymax6 on ${Platform.operatingSystem} ${Platform.operatingSystemVersion}');
+
+  var cmd = '';
+
+  while( cmd != 'Q' ) {
+    executeCommand(cmd);
+    cmd = readCommand();
+  }
+
+  print('All done!');
 }
