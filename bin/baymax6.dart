a4a6f87103b3c08c78bd28cb0b39fc119915f6d8
gianninasd <angelone55@hotmail.com>
2022-07-26 22:36:43 -0400

    added httpclient

--- bin/baymax6.dart
+++ bin/baymax6.dart
@@ -1,16 +1,39 @@
+import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
 
 //import 'package:baymax6/baymax6.dart' as baymax6;
 
+// retrieves a list of toys
+void getToyList() async {
+  var client = HttpClient();
+
+  try {
+    Map<String, String> headers = HashMap();
+    headers.putIfAbsent('Accept', () => 'application/json');
+
+    HttpClientRequest req = await client.getUrl(Uri.parse('https://2nif3zuj43.execute-api.us-east-2.amazonaws.com/default/toys1'));
+    HttpClientResponse response = await req.close();
+    // Process the response
+    final stringData = await response.transform(utf8.decoder).join();
+    print(stringData);
+  }
+  on Exception catch (ex) {
+    print('oops: $ex');
+  }
+  finally {
+    client.close();
+  }
+}
+
 // run the specific business command
-void executeCommand(String command) {
+void executeCommand(String command) async {
   switch (command) {
     case 'G':
       print('not implemented yet');
       break;
     case 'L':
-      print('not implemented yet');
+      getToyList();
       break;
   }
 }
@@ -23,12 +46,12 @@ String readCommand() {
 
   var x = AsciiDecoder();
   var cmd = x.convert([charCode]);
-  print('it was: $cmd');
+  //print('it was: $cmd');
   return cmd.toUpperCase();
 }
 
 // application starts here
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   print('----------------------------------------');
   print('Running Baymax6 on ${Platform.operatingSystem} ${Platform.operatingSystemVersion}');
 
@@ -39,5 +62,5 @@ void main(List<String> arguments) {
     cmd = readCommand();
   }
 
-  print('All done!');
+  print('Exiting!');
 }
